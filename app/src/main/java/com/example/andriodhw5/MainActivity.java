08a60e39255c85a4a52ca6b8fb0ce008080ce617
karta571110 <karta571110@gmail.com>
2019-10-21 03:06:23 +0800

    變數設置完畢

--- app/src/main/java/com/example/andriodhw5/MainActivity.java
+++ app/src/main/java/com/example/andriodhw5/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.andriodhw5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText inputyear = (EditText) findViewById(R.id.inputyear);
+        final EditText inputmonth = (EditText) findViewById(R.id.inputmonth);
+        final EditText inputday = (EditText) findViewById(R.id.inputday);
+        final EditText houredit = (EditText) findViewById(R.id.houredit);
+        final EditText minedit = (EditText) findViewById(R.id.minedit);
+        final EditText secedit = (EditText) findViewById(R.id.secedit);
+        TextView ouputtext = (TextView) findViewById(R.id.outputtext);
+        Button displaytime = (Button) findViewById(R.id.displaytime);
+
+        displaytime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!("".equals(inputyear.getText().toString())) &&
+                        "".equals(inputmonth.getText().toString()) &&
+                        "".equals(inputday.getText().toString()) &&
+                        "".equals(houredit.getText().toString()) &&
+                        "".equals(minedit.getText().toString()) &&
+                        "".equals(secedit.getText().toString())) {
+
+
+
+                }
+            }
+        });
     }
 }
